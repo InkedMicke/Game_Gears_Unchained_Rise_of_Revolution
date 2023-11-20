9ae7445d91ce578fc28158ad4b712c8654728342
KevinBarba2A <kevin_barba_b-ts3dv1ma2223@cev.com>
2023-11-20 17:14:28 +0100

    GodModeInProcess

--- Assets/!WeAreAthomic/SCRIPTS/Player_Scripts/MainCRailGrindSystem.cs
+++ Assets/!WeAreAthomic/SCRIPTS/Player_Scripts/MainCRailGrindSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using _WeAreAthomic.SCRIPTS.Genericos_Scripts;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
