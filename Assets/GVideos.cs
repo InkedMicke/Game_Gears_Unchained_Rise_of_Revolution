3ed09a87e68be56986527d5c5dba5eb13bb0bd74
KevinBarba2A <kevin_barba_b-ts3dv1ma2223@cev.com>
2023-12-13 21:33:18 +0100

    VideoSkipIntro

---
+++ Assets/GVideos.cs
@@ -0,0 +1,43 @@
+using _WeAreAthomic.SCRIPTS.Genericos_Scripts;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class GVideos : MonoBehaviour
+{
+    [SerializeField] private GLoadScene sceneLoader;
+
+    [SerializeField] private VideoPlayer videoPlayer;
+
+    [SerializeField] private List<VideoClip> videos;
+
+    [SerializeField] private bool loadSceneOnFinish;
+
+    [SerializeField] private string sceneString = "sample";
+
+    private int _currentVideo;
+
+    private void Start()
+    {
+        videoPlayer.clip = videos[_currentVideo];
+        videoPlayer.Play();
+        _currentVideo++;
+    }
+
+    public void NextVideo()
+    {
+        if (_currentVideo < videos.Count)
+        {
+            videoPlayer.clip = videos[_currentVideo];
+            videoPlayer.Play();
+            _currentVideo++;
+        }
+        else
+        {
+            if(loadSceneOnFinish)
+            {
+                sceneLoader.LoadScene(sceneString);
+            }
+        }
+    }
+}
