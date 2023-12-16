7b7942ea4713cfb66fa2c4b182a2cbedc1e36d1a
InkedMicke <michael_martin_b-ts3dv1mb2223@cev.com>
2023-12-17 00:12:28 +0100

    Update C_DropdownFunctions.cs

--- Assets/!WeAreAthomic/SCRIPTS/C_DropdownFunctions.cs
+++ Assets/!WeAreAthomic/SCRIPTS/C_DropdownFunctions.cs
@@ -8,26 +8,46 @@ public class C_DropdownFunctions : MonoBehaviour
     [SerializeField] private GameObject[] objectsES;
     [SerializeField] private GameObject[] objectsEN;
 
-  
+    [SerializeField] TMP_Dropdown dropdown;
+    [SerializeField] List<string> idiomasES;
+    [SerializeField] List<string> idiomasEN;
 
+    
+
+    private void Start()
+    {
+        idiomasES.Add("ESPAÑOL");
+        idiomasES.Add("INGLES");
+
+        idiomasEN.Add("ENGLISH");
+        idiomasEN.Add("SPANISH");
+       
+
+        dropdown.AddOptions(idiomasES);
+
+    }
 
     public void OnChangeLanguage (int index)
     {
        
-        switch (index)
-            
+       
+        switch (index)   
         {
-            case 0 :
-               
+            case 0 : //Español (DESDE ESPAÑOL)
+             
+                dropdown.ClearOptions();
+                dropdown.AddOptions(idiomasES);
                 ActivarObjetosES();
                 DesactivarObjetosEN();    
                 
                     break;
 
-            case 1:
-               
+            case 1: //Ingles (DESDE ESPAÑOL)
+                dropdown.ClearOptions();
+                dropdown.AddOptions(idiomasEN);
                 ActivarObjetosEN();
                 DesactivarObjetosES();
+                
 
                 break;
                 
