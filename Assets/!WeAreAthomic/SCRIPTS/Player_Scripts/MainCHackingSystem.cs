ce92153fc28884b34c533d9fdcba1b78d0225444
KevinBarba2A <kevin_barba_b-ts3dv1ma2223@cev.com>
2024-01-09 16:43:41 +0100

    Revert "ArregloLab"
    This reverts commit 2003a36b33f67d5cb209173df38bb98bc0eaf281.

--- Assets/!WeAreAthomic/SCRIPTS/Player_Scripts/MainCHackingSystem.cs
+++ Assets/!WeAreAthomic/SCRIPTS/Player_Scripts/MainCHackingSystem.cs
@@ -66,7 +66,7 @@ namespace _WeAreAthomic.SCRIPTS.Player_Scripts
                     }
                 }
 
-                if (typeOfHacked == TypeOfHacked.botonActivaAlgo || typeOfHacked == TypeOfHacked.botonPuerta)
+                if (typeOfHacked == TypeOfHacked.prop)
                 {
                     FixPosition();
                     EnableHackAnim();
--- Assets/!WeAreAthomic/SCRIPTS/Props_Scripts/ButtonInteractable.cs
+++ Assets/!WeAreAthomic/SCRIPTS/Props_Scripts/ButtonInteractable.cs
@@ -7,8 +7,7 @@ namespace _WeAreAthomic.SCRIPTS.Props_Scripts
 {
     public enum TypeOfHacked
     {
-        botonPuerta,
-        botonActivaAlgo,
+        prop,
         soldier
     }
 
@@ -84,10 +83,7 @@ namespace _WeAreAthomic.SCRIPTS.Props_Scripts
             if (canHack)
             {
                 seActivanCuandoTerminaElHack.Invoke();
-                if (typeOfHacked == TypeOfHacked.botonPuerta)
-                {
-                    canHack = false;
-                }
+                canHack = false;
                 isActive = false;
             }
         }
