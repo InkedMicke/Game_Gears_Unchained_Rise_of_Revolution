182c0eff6fcad0fa97c48dae577ad739175a2427
Michael Martin Bedmar <131700123+InkedMicke@users.noreply.github.com>
2024-01-11 17:17:32 +0100

    Bala_Soldier

--- Assets/!WeAreAthomic/SCRIPTS/Enemi_Scripts/GreenSoldierBulletHitBox.cs
+++ Assets/!WeAreAthomic/SCRIPTS/Enemi_Scripts/GreenSoldierBulletHitBox.cs
@@ -7,14 +7,28 @@ namespace _WeAreAthomic.SCRIPTS.Enemi_Scripts
     public class GreenSoldierBulletHitBox : MonoBehaviour
     {
         [SerializeField] private float damage = 5;
-
+        [SerializeField] private GameObject hitChispas;
         private void OnTriggerEnter(Collider other)
         {
+            
+            Instantiate(hitChispas,transform.position,Quaternion.identity);
+
             if (other.TryGetComponent(out IDamageable damageable))
             {
                 damageable.Damage(damage);
+                
             }
+           
             Destroy(gameObject);
         }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            Instantiate(hitChispas, transform.position, Quaternion.identity);
+
+
+            Destroy(gameObject);
+        }
+
     }
 }
