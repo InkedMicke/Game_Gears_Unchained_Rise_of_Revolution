676d9d1085f8e2724b370fadd149f529469fb7a6
InkedMicke <michael_martin_b-ts3dv1mb2223@cev.com>
2024-01-25 00:57:02 +0100

    Cositas

--- Assets/GTweenDoRotate.cs
+++ Assets/GTweenDoRotate.cs
@@ -7,14 +7,38 @@ public class GTweenDoRotate : MonoBehaviour
 {
     [SerializeField] private Vector3 target;
     [SerializeField] private bool loop;
+    [SerializeField]private bool WantToRotateStart;
     [SerializeField] private LoopType loopType;
     [SerializeField] private float duration;
     [SerializeField] private RotateMode rotateMode;
     [SerializeField] private Ease ease;
 
+    
+
     private void Start()
     {
-        if(loop)
+        if(WantToRotateStart)
+        {
+            if (loop)
+            {
+                transform.DORotate(target, duration, rotateMode)
+                    .SetLoops(-1, loopType)
+                    .SetRelative()
+                    .SetEase(ease);
+            }
+            else
+            {
+                transform.DORotate(target, duration, rotateMode)
+                    .SetRelative()
+                    .SetEase(ease);
+            }
+        }
+        
+
+    }
+    public void StartRotate()
+    {
+        if (loop)
         {
             transform.DORotate(target, duration, rotateMode)
                 .SetLoops(-1, loopType)
@@ -27,6 +51,10 @@ public class GTweenDoRotate : MonoBehaviour
                 .SetRelative()
                 .SetEase(ease);
         }
+    }
 
+    public void StopRotate()
+    {
+        transform.DOKill();
     }
 }
