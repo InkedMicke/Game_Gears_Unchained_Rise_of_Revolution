f62c322de0e0d1a6dffb648e788cc47ebbb3e6cb
KevinBarba2A <145692600+KevinBarba2A@users.noreply.github.com>
2024-02-23 19:28:30 +0100

    broom

---
+++ Assets/!WeAreAthomic/SCRIPTS/Enemi_Scripts/Blue/BroomAnimator.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Broom
+{
+    public class BroomAnimator : MonoBehaviour
+    {
+        Animator m_anim;
+
+        private void Awake()
+        {
+            m_anim = GetComponent<Animator>();
+        }
+
+        public void SetShieldCount(int count)
+        {
+            m_anim.SetInteger("ShieldCount", count);
+        }
+
+        public void SetJumpAttackCount(int count)
+        {
+            m_anim.SetInteger("JumpAttackCount", count);
+        }
+
+        public void SetThurstCount(int count)
+        {
+            m_anim.SetInteger("ThurstCount", count);
+        }
+
+        public void SetMolinilloCount(int count)
+        {
+            m_anim.SetInteger("MolinilloCount", count);
+        }
+
+        public void SetCrossAttackCount(int count)
+        {
+            m_anim.SetInteger("CrossAttackCount", count);
+        }
+
+        public void SetIsWalking(bool isWalking)
+        {
+            m_anim.SetBool("isWalking", isWalking);
+        }
+
+        public void SetIsStunned(bool isStunned)
+        {
+            m_anim.SetBool("isStuned", isStunned);
+        }
+
+        public void TriggerHurt()
+        {
+            m_anim.SetTrigger("hurt");
+        }
+        public void TriggerShoot()
+        {
+            m_anim.SetTrigger("shoot");
+        }
+
+    }
+}
---
+++ Assets/!WeAreAthomic/SCRIPTS/Enemi_Scripts/Blue/BroomHurtBox.cs
@@ -0,0 +1,59 @@
+using _WeAreAthomic.SCRIPTS.Interfaces_Scripts;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Broom
+{
+    public class BroomHurtBox : Broom, IDamageable
+    {
+        private Coroutine m_c_waitForEndShield;
+
+        [NonSerialized] public bool CanReceiveDamage;
+
+        [SerializeField] float maxHealth;
+        [NonSerialized] public float CurrentHealth;
+
+        []
+
+        private void Awake()
+        {
+            CurrentHealth = maxHealth;
+        }
+
+        public void Damage(float dmg)
+        {
+            if (CanReceiveDamage)
+            {
+                if (!CheckForDeath())
+                {
+                    CurrentHealth -= dmg;
+                }
+            }
+            else
+            {
+                broomAnimator.SetShieldCount(1);
+            }
+        }
+
+        private IEnumerator WaitForDisableShield()
+        {
+            yield return new WaitForSeconds(1f);
+        }
+
+        private bool CheckForDeath()
+        {
+            if (CurrentHealth <= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void SetCanReceiveDamage(bool canReceiveDamage)
+        {
+            CanReceiveDamage = canReceiveDamage;
+        }
+    }
+}
