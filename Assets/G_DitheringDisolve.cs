fd2f7271ce2d071af02ccd6d066096a27ccb687a
InkedMicke <131700123+InkedMicke@users.noreply.github.com>
2024-03-01 19:47:08 +0100

    ExplosionBustos

---
+++ Assets/G_DitheringDisolve.cs
@@ -0,0 +1,30 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G_DitheringDisolve : MonoBehaviour
+{
+
+    [SerializeField] private Material _mat;
+
+    [SerializeField] float ditherTime = 5f;
+
+    MeshRenderer[] renderers;
+
+    // Start is called before the first frame update
+    public void StartDisolveDithering()
+    { 
+        renderers = GetComponentsInChildren<MeshRenderer>();
+        Material newMat = new(_mat);
+
+        foreach (var renderer in renderers)
+        {
+            renderer.sharedMaterial = newMat;
+        }
+        newMat.DOFloat(0f, "_DitherThreshold", ditherTime).SetEase(Ease.InQuint).OnComplete(() => { Destroy(transform.parent.gameObject); });
+    }
+
+   
+
+}
